9bbb59d302edbde9b4f888a3a259071b53501052
Luís Soares <lsoares@gmail.com>
2019-12-03 00:39:17 +0000

    Solve part 2 of day 1

--- src/test/kotlin/TestDay1Part2.kt
+++ src/test/kotlin/TestDay1Part2.kt
@@ -4,21 +4,21 @@ import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.MethodSource
 import java.util.*
 
-object TestDay1Part1 {
+object TestDay1Part2 {
 
     @JvmStatic
-    fun input() = setOf(12 to 2, 1969 to 654, 100756 to 33583)
+    fun input() = setOf(14 to 2, 1969 to 966, 100756 to 50346)
 
     @ParameterizedTest
     @MethodSource("input")
     fun `basic input`(value: Pair<Int, Int>) {
-        assertEquals(value.second, Day1Part1.calc(value.first))
+        assertEquals(value.second, Day1Part2.calc(value.first))
     }
 
     @Test
     fun exercise() {
         val inputStream = this::class.java.getResourceAsStream("day1.txt")
-        val sum = Scanner(inputStream).asSequence().map { Day1Part1.calc(it.toInt()) }.sum()
-        assertEquals(3423279, sum)
+        val sum = Scanner(inputStream).asSequence().map { Day1Part2.calc(it.toInt()) }.sum()
+        assertEquals(5132018, sum)
     }
 }
