a629f38f91d2069f6a1d2171fcc11b8e0ddb7c0a
Luís Soares <lsoares@gmail.com>
2019-12-07 00:03:17 +0000

    Solve Part 4, Part 2

--- src/main/kotlin/Day4.kt
+++ src/main/kotlin/Day4.kt
@@ -1,10 +1,25 @@
 object Day4 {
 
-    fun isValid(input: Int) =
-        input in (111111..999999) && input.hasConsecutive() && input.increases()
+    fun isValid1(input: Int) =
+        input in (100000..999999) && input.countConsecutivePairs() > 0 && input.increases()
 
-    private fun Int.hasConsecutive() =
-        toString().zipWithNext().any { it.first == it.second }
+    fun isValid2(input: Int): Boolean {
+        return input in (100000..999999) && input.getCounters().contains(1) && input.increases()
+    }
+
+    private fun Int.countConsecutivePairs() =
+        toString().zipWithNext().count { it.first == it.second }
+
+    private fun Int.getCounters(): List<Int> {
+        val res = IntArray(6)
+        var consec = 0
+        for (i in 1 until toString().length) {
+            if (toString()[i] == toString()[i - 1]) {
+                res[consec]++
+            } else consec++
+        }
+        return res.toList()
+    }
 
     private fun Int.increases() =
         toString().zipWithNext().all { it.first <= it.second }
--- src/test/kotlin/Day4Test.kt
+++ src/test/kotlin/Day4Test.kt
@@ -5,20 +5,37 @@ import org.junit.jupiter.params.provider.ValueSource
 
 object Day4Test {
 
-    @Test
-    fun `find solution`() {
-        assertEquals(1873, (136760..595730).count(Day4::isValid))
-    }
-
     @ParameterizedTest
     @ValueSource(ints = [123356, 111111, 112233])
-    fun `test is valid`(test: Int) {
-        assertTrue(Day4.isValid(test))
+    fun `test part 1 - valid`(test: Int) {
+        assertTrue(Day4.isValid1(test))
     }
 
     @ParameterizedTest
     @ValueSource(ints = [111110, 223450, 123789])
-    fun `test is invalid`(test: Int) {
-        assertFalse(Day4.isValid(test))
+    fun `test part 1 - invalid`(test: Int) {
+        assertFalse(Day4.isValid1(test))
+    }
+
+    @Test
+    fun `find solution part 1`() {
+        assertEquals(1873, (136760..595730).count(Day4::isValid1))
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [112233, 111122])
+    fun `test part 2 - valid`(test: Int) {
+        assertTrue(Day4.isValid2(test))
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [123444, 123456, 111222])
+    fun `test part 2 - invalid`(test: Int) {
+        assertFalse(Day4.isValid2(test))
+    }
+
+    @Test
+    fun `find solution part 2`() {
+        assertEquals(878, (136760..595730).count(Day4::isValid2))
     }
 }
